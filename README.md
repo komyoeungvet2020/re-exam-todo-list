1594e6fad11258dfec9f3b816e2cbf99527c0f45
khomyoeung.vet2020 <khomyoeung.vet@camsolution.biz>
2022-10-12 10:35:59 +0700

    todo list

---
+++ README.md
@@ -0,0 +1,23 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+Install node module
+
+### `npm install`
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+Runs API command 
+
+### `npm run api`
+
+Open (http://localhost:3030/list_api) && (http://localhost:3030/list_api) to view it in your APIs.
+
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import Header from "./component/Header";
+import Home from './component/Pages/Home';
+import Computer from './component/Products/Computer';
+import Watch from './component/Products/Watch';
+import Phone from './component/Products/Phone';
+import DetailComputer from './component/Products/DetailComputer';
+import DetailPhone from './component/Products/DetailPhone';
+import DetailWatch from './component/Products/DetailWatch';
+
+function App() {
+
+ReactDOM.render(
+  <React.StrictMode>
+       <Router>
+          <Routes>
+          <Route exact path="/" element={<Header />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/computer" element={<Computer />}/>
+            <Route path="/watch" element={<Watch />} />
+            <Route path="/phone" element={<Phone />} />
+            <Route path="/detailComputer/:itemDetail" element={<DetailComputer/>}/>
+            <Route path="/detailPhone/:itemDetail1" element={<DetailPhone/>}/>
+            <Route path="/detailWatch/:itemDetail2" element={<DetailWatch/>}/>
+
+          </Routes>
+        </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+}
+
+export default App;
